52629b1f2036363c2e1ffadbac763704192aab53
전다훈 <dahunee1111@gmail.com>
2026-04-20 12:35:14 +0900

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,12 @@
+from fastapi import FastAPI
+from app.routers import upload, analysis, history
 
+app = FastAPI()
+
+app.include_router(upload.router)
+app.include_router(analysis.router)
+app.include_router(history.router)
+
+@app.get("/")
+def root():
+    return {"message": "Server is running"}
