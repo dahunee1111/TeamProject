c87fb17a2037e6c9f2eea274804fc1ce0c876fe9
전다훈 <dahunee1111@gmail.com>
2026-04-20 13:59:20 +0900

    Update history.py

--- backend/app/routers/history.py
+++ backend/app/routers/history.py
@@ -1,1 +1,10 @@
+from fastapi import APIRouter
 
+router = APIRouter(tags=["History"])
+
+@router.get("/history")
+def get_history():
+    return {
+        "status": "success",
+        "data": []
+    }
